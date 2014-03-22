4b1cbf312316fd59caf810e38b43b53d71ffe8fb
Thanasis Polychronakis <thanpolas@gmail.com>
2014-03-22 21:38:40 +0200

    add cookie module too...

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
-
+var Modernizr = require('modernizr/feature-detects/cookies');
 var Modernizr = require('modernizr/src/Modernizr');
 
 console.log('Modernizr:', !!Modernizr);
+console.log('Modernizr:', Modernizr.cookies);
